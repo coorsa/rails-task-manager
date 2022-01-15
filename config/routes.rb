9032f0b01513217bdb2f5e5ef423e1b13954288b
Niklas Coors <coors.niklas@gmail.com>
2022-01-15 11:53:22 +0100

    added create functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    get 'tasks', to: 'tasks#index'
-  end
+  # read all route
+  get 'tasks', to: 'tasks#index'
+  # create routes
+  get 'tasks/new', to: 'tasks#new'
+  # post new form
+  post 'tasks', to: 'tasks#create'
+  # read just one route
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
