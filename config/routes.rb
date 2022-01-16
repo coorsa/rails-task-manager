a7019efba4054719720ab1f64859082a134143fb
Niklas Coors <coors.niklas@gmail.com>
2022-01-16 18:51:27 +0100

    added update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   # read all route
   get 'tasks', to: 'tasks#index'
   # create routes
-  get 'tasks/new', to: 'tasks#new'
-  # post new form
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post new form route
   post 'tasks', to: 'tasks#create'
   # read just one route
   get 'tasks/:id', to: 'tasks#show', as: :task
+  # edit a task - get HTML form details route
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # update a task - patch route
+  patch 'tasks/:id', to: 'tasks#update'
 end
